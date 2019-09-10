98177c35812f840cb5e0205585806087fad5b03e
Fernando Luna <337n4nd0@gmail.com>
2019-09-09 22:04:20 -0400

    update zip bundle configuration for beanstalk

--- scripts/create_zip_for_aws.sh
+++ scripts/create_zip_for_aws.sh
@@ -4,4 +4,4 @@ if [ -f docker/DockerFile ]; then
     cp docker/Dockerfile ./
 fi
 
-git archive -v -o tantakatu-api-bundle.zip --format=zip HEAD
+git archive -v -o tantakatu-api-bundle-$(date +"%d%m%y%HH%mm").zip --format=zip HEAD
